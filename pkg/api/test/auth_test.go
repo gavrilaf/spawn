bb8d36e40b049f73dc328b93808c072184410e5a
eugenf <eugenf78@gmail.com>
2018-04-16 07:53:05 +0300

    implementing api tests (wip)

--- pkg/api/test/auth_test.go
+++ pkg/api/test/auth_test.go
@@ -3,31 +3,30 @@ package test
 import (
 	"bytes"
 	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 
 	//"github.com/stretchr/testify/require"
 	//"github.com/fatih/structs"
 	"github.com/satori/go.uuid"
+
+	"github.com/stretchr/testify/assert"
 	"testing"
 	//"github.com/gin-gonic/gin"
 
 	//"github.com/gavrilaf/spawn/pkg/api/auth"
 	"github.com/gavrilaf/spawn/pkg/cryptx"
-	//"github.com/gavrilaf/spawn/pkg/api/config"
-	//"github.com/gavrilaf/spawn/pkg/senv"
 )
 
 func Test_SignUp(t *testing.T) {
-	engine := createTestEngine(t)
+	teng := createTestEngine(t)
 
 	deviceID := "device-111"
 	username := uuid.NewV4().String()
-	client := engine.getClient(t)
+	client := teng.getClient(t)
 	sign := cryptx.GenerateSignature(client.ID+deviceID+username, client.Secret)
 
-	correct_boby := map[string]string{
+	correct := map[string]string{
 		"client_id":   client.ID,
 		"device_id":   deviceID,
 		"device_name": "Test device",
@@ -35,11 +34,28 @@ func Test_SignUp(t *testing.T) {
 		"password":    "password",
 		"signature":   string(sign)}
 
+	invalid_param := map[string]string{
+		"client_id": client.ID,
+		"device_id": deviceID}
+
+	invalid_sign := map[string]string{
+		"client_id":   client.ID,
+		"device_id":   deviceID,
+		"device_name": "Test device",
+		"username":    username,
+		"password":    "password",
+		"signature":   "11111"}
+
+	already_registered := correct
+
 	tests := []struct {
 		body          map[string]string
 		expected_code int
 	}{
-		{correct_boby, 200},
+		{correct, 200},
+		{invalid_param, 400},
+		{invalid_sign, 500},
+		{already_registered, 500},
 	}
 
 	for _, tt := range tests {
@@ -48,8 +64,43 @@ func Test_SignUp(t *testing.T) {
 		req.Header.Set("Content-Type", "application/json")
 		w := httptest.NewRecorder()
 
-		engine.engine.ServeHTTP(w, req)
+		teng.engine.ServeHTTP(w, req)
 
 		assert.Equal(t, tt.expected_code, w.Code)
 	}
+}
+
+func Test_SignIn(t *testing.T) {
+	teng := createTestEngine(t)
+
+	correct := teng.registerUser(t)
+	correct["auth_type"] = "password"
+
+	invalid_param := mapDeepCopy(correct)
+	delete(invalid_param, "device_id")
+
+	invalid_sign := mapDeepCopy(correct)
+	invalid_param["signature"] = "11111"
+
+	tests := []struct {
+		name          string
+		body          map[string]string
+		expected_code int
+	}{
+		{"correct", correct, 200},
+		{"invalid param", invalid_param, 400},
+		{"invalid sign", invalid_sign, 500},
+	}
+
+	for _, tt := range tests {
+		jbody, _ := json.Marshal(tt.body)
+		req, _ := http.NewRequest("POST", "/auth/login", bytes.NewReader(jbody))
+		req.Header.Set("Content-Type", "application/json")
+		w := httptest.NewRecorder()
+
+		teng.engine.ServeHTTP(w, req)
+
+		assert.Equal(t, tt.expected_code, w.Code, tt.name)
+	}
+
 }
--- pkg/api/test/test_helper.go
+++ pkg/api/test/test_helper.go
@@ -1,16 +1,31 @@
 package test
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+
 	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/satori/go.uuid"
 
 	"github.com/gavrilaf/spawn/pkg/api"
 	"github.com/gavrilaf/spawn/pkg/api/config"
+	"github.com/gavrilaf/spawn/pkg/cryptx"
 	db "github.com/gavrilaf/spawn/pkg/dbx/mdl"
 	"github.com/gavrilaf/spawn/pkg/senv"
 )
+
+func mapDeepCopy(p map[string]string) map[string]string {
+	new := make(map[string]string)
+	for k, v := range p {
+		new[k] = v
+	}
+	return new
+}
 
 const (
 	tClientID = "client-test-01"
@@ -36,4 +51,29 @@ func (self testEngine) getClient(t *testing.T) *db.Client {
 	p, err := self.api.ReadModel.FindClient(tClientID)
 	require.Nil(t, err)
 	return p
+}
+
+func (self testEngine) registerUser(t *testing.T) map[string]string {
+	deviceID := "device-111"
+	username := uuid.NewV4().String()
+	client := self.getClient(t)
+	sign := cryptx.GenerateSignature(client.ID+deviceID+username, client.Secret)
+
+	body := map[string]string{
+		"client_id":   client.ID,
+		"device_id":   deviceID,
+		"device_name": "Test device",
+		"username":    username,
+		"password":    "password",
+		"signature":   string(sign)}
+
+	jbody, _ := json.Marshal(body)
+	req, _ := http.NewRequest("PUT", "/auth/register", bytes.NewReader(jbody))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	self.engine.ServeHTTP(w, req)
+	require.Equal(t, 200, w.Code)
+
+	return body
 }
